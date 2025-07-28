59cf5c328a3819ff5a725850d122c9d307e1ee84
Saniya Phanse <saniyaphanse@gmail.com>
2025-07-28 17:29:36 +0530

    created functional and class component

---
+++ session1/src/components/HelloClassComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloClassComponent = () => {
+  return <h1>Hello from class component</h1>;
+};
+
+export default HelloClassComponent;
---
+++ session1/src/components/HelloFunctional.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloFunctional = () => {
+  return <h1>Helllo from functional component</h1>;
+};
+
+export default HelloFunctional;
