aaa3b6e6c9bd40abb4a2c9ad669badc246cef4c1
Saniya Phanse <saniyaphanse@gmail.com>
2025-07-28 17:50:30 +0530

    added navbar

--- session1/src/App.jsx
+++ session1/src/App.jsx
@@ -4,10 +4,12 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import HelloFunctional from "./components/HelloFunctional";
 import HelloClassComponent from "./components/HelloClassComponent";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <HelloFunctional />
       <HelloClassComponent />
     </>
---
+++ session1/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="div">
+        <a href="">Home</a>
+        <a href="">About US</a>
+        <a href="">Contact</a>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
